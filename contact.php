71871d7765eb6a2df3ed0ad7981f64a35e1361f1
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-07-05 16:21:17 +0100

    starting on contact form

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="styles/contact.css" media="all"/>
     <script
       src="https://kit.fontawesome.com/dc7bf59d0f.js"
       crossorigin="anonymous"
@@ -14,3 +15,12 @@
   </head>
   <body>
     <?php include('header.php'); ?>
+
+    <form action="mail.php" method="POST">
+<p>Name</p> <input type="text" name="name">
+<p>Email</p> <input type="text" name="email">
+<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
+<input type="submit" value="Send"><input type="reset" value="Clear">
+</form>
+
+  </body>
