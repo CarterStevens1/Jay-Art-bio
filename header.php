01739a92e912276588faff6b25003279350424a7
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-07-05 14:40:35 +0100

    convert to php

---
+++ header.php
@@ -0,0 +1,29 @@
+<div class="nav">
+
+<div class="nav-links">
+  <div class=" logo-box">
+    <img class="logo" src="images/JayEnvyLogo2.png" alt="Jay Envy Logo" />
+  </div>
+  <span class="burger" style="font-size: 30px; cursor: pointer"
+    >&#9776;</span
+  >
+  <div class=" links sidenav">
+    <a href="javascript:void(0)" class="closebtn">&times;</a>
+    <a href="#" class="nav-item">Home</a>
+    <a href="#" class="nav-item">About</a>
+    <a href="contact.php" class="nav-item">Contact</a>
+    <a href="#" class="nav-item">Store</a>
+    <a href="#" class="nav-item">Print</a>
+
+    
+      <div class="social">
+        <a class="icons" href=""><i class="fab fa-facebook"></i></a>
+        <a class="icons" href=""><i class="fab fa-twitter"></i></a>
+        <a class="icons" href=""><i class="fab fa-instagram"></i></a>
+        <a class="icons" href=""> <i class="fab fa-linkedin-in"></i></a>
+      </div>
+    
+    </td>
+  </div>
+</div>
+</div>
